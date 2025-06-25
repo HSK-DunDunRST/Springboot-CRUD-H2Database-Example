81e523b944507665702d5ab3c4ad25902ce85e90
HSK-DunDunRST <gon5073@naver.com>
2025-06-25 22:23:35 +0900

    Create Project and Convert .properties to .yml

---
+++ src/test/java/com/hsk/crud/h2database/H2databaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsk.crud.h2database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2databaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
